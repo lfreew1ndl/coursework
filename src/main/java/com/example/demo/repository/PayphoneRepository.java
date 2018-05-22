d5a45482fe1edf419a5947bba17b703994d6fc7f
Salim <salimsa1@mail.ru>
2018-05-22 14:29:23 +0300

    first commit after change front

--- src/main/java/com/example/demo/repository/PayphoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Payphone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PayphoneRepository extends JpaRepository<Payphone,Long> {
-}
